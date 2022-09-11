f273540dd826170ad8bc5045778ef46cc849c511
mttt <groobytt@gmail.com>
2022-09-11 19:05:01 +0200

    add drawing grid function with grid layout

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
 const okBtn=document.querySelector('#ok-button')
+const BOX = document.querySelector("#drawing-space")
+const BOXsize = BOX.clientWidth
+
+
+
 okBtn.addEventListener('click',()=>{
     deleteGrid()
     const size=document.querySelector("#grid-size").value
     drawGrid(size)
 })
+
+
+/* DELETING GRID */
 function deleteGrid(){
      let toDelete = document.querySelectorAll(".cell")
      toDelete.forEach(element=>{
@@ -12,32 +20,22 @@ function deleteGrid(){
      })
 }
 
-const BOX = document.querySelector("#drawing-space")
-const BOXsize = BOX.clientWidth
 
+
+
+/* DRAWING GRID */
 function drawGrid(size){
     
     const cell = document.createElement('div')
     cell.classList.add('cell')
-
-   // let borderSize=BOX.style.borderWidth
-    
-
-    var cellSize=(BOXsize-(2*2*size))/(size)
-    console.log('BOX SIZE:',BOXsize)
-    console.log('CELL SIZE:',cellSize)
-    cell.style.width=`${cellSize}px`
-    cell.style.height=`${cellSize}px`
-    
+    BOX.style.gridTemplateColumns=`repeat(${size},1fr)`
     for(let i=0;i<size*size;i++){
         let clonedCell=cell.cloneNode(false)
         BOX.appendChild(clonedCell)
-       // console.dir('border size:',borderSize)
+       
     }
-        
-        
-
 }
+
 
 
  
