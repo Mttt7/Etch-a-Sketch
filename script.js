1ae6773eda6e2a1f9c7a17a0906e3ef7eff80f3c
mttt <groobytt@gmail.com>
2022-09-14 10:10:14 +0200

    add rainbow mode and fix drawing

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+let currentMode = 0
+
 const BOX = document.querySelector("#drawing-space")
 const BOXsize = BOX.clientWidth
 const sizeOutcome = document.querySelector("#size-display")
 const slider = document.querySelector("#grid-size")
 let output = document.querySelector("#size-display")
 const resetBtn=document.querySelector("#reset-button")
+const rainbowLetters=document.querySelectorAll('.rainbow-letter')
+const rainbowWrapper=document.querySelector("#rainbow-mode")
+
+rainbowWrapper.addEventListener('click',changeLetterColor)
+
+
+
+let mouseDown=false
+BOX.addEventListener('mousedown',function(e){
+    e.preventDefault()
+    mouseDown=true
+})
+BOX.addEventListener('mouseup',function(e){
+    e.preventDefault()
+    mouseDown=false
+})
 
 /* DISPLAYING SIZE */
 output.innerHTML=slider.value
 drawGrid(slider.value)
 slider.oninput=function(){
+    
     deleteGrid()
     output.innerHTML=this.value
     drawGrid(this.value)
@@ -35,6 +54,7 @@ function drawGrid(size){
        
     }
     addListeners()
+    
 }
 
 /* ----------------------------- */
@@ -43,14 +63,57 @@ function addListeners(){
     
     let cells=document.querySelectorAll('.cell')
     cells.forEach((cell)=>{
-    cell.addEventListener('mouseenter' ,function(){
-        cell.style.backgroundColor='black'
+    cell.addEventListener('mousedown',changeColor)
+    cell.addEventListener('mouseover',changeColor)
+    function changeColor(e){
+        e.preventDefault()
+        if(e.type==='mouseover' && !mouseDown){
+            
+            return
+        } 
+        if(currentMode==0) cell.style.backgroundColor='black'
+        else if(currentMode==1) cell.style.backgroundColor=randomColor()
         
-    })
-  
+        
+        
+        
+    }
 })
 }
 
 
+/* RAINBOW MODE */
+
+/*0-black  1-rainbow */
+
+
+
+function changeLetterColor(){
+    if(currentMode==0){
+        console.log(currentMode)
+    rainbowLetters.forEach((letter)=>{
+        letter.style.color=randomColor()
+        
+    })
+    currentMode=1
+    }
+else if(currentMode==1){
+    
+    rainbowLetters.forEach((letter)=>{
+        letter.style.color='black'
+        
+    })
+    currentMode=0
+}
+}
+function randomColor(){
+    
+    
+    return rgb(Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256))
+}
+
+function rgb(r,g,b){
+    return `rgb(${Math.floor(r)},${Math.floor(g)},${Math.floor(b)})`
+}
 
  
